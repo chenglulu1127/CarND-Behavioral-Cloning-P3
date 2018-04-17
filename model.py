6c96cadb35e82fe2e87cfbd6284e4ad2f7529777
Lulu Cheng <lulu@uber.com>
2018-04-17 10:06:21 +0200

    full run

---
+++ model.py
@@ -0,0 +1,103 @@
+import csv
+import cv2
+import numpy as np
+import sklearn
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Lambda, Activation
+from keras.layers.convolutional import Convolution2D, Cropping2D, ZeroPadding2D, MaxPooling2D
+from keras.optimizers import Adam
+from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
+
+# define generator function
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = batch_sample[0]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                
+                # augment side camera images
+                # create adjusted steering measurements for the side camera images
+                correction = 0.5
+                steering_left = center_angle + correction
+                steering_right = center_angle - correction
+                # read side camera images
+                img_left = cv2.imread(batch_sample[1])
+                img_right = cv2.imread(batch_sample[2])
+                
+                images.append(center_image)
+                images.append(img_left)
+                images.append(img_right)
+                angles.append(center_angle)
+                angles.append(steering_left)
+                angles.append(steering_right)
+            
+            
+            # augment flipped images data
+            augmented_images, augmented_angles = [], []
+            for image, angle in zip(images, angles):
+                augmented_images.append(image)
+                augmented_angles.append(angle)
+                augmented_images.append(cv2.flip(image,1))
+                augmented_angles.append(angle*-1.0)
+
+            X_train = np.array(augmented_images)
+            y_train = np.array(augmented_angles)
+
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+samples = []
+
+# read training dataset
+with open('../simulator_data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        samples.append(line)
+
+# train validation set split 0.2, 0.8
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+
+
+# build model
+model = Sequential()
+model.add(Lambda(lambda x: x/255.0 - 0.5,
+        input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((40,20), (0,0))))
+model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(36,5,5,subsample=(2,2), activation="relu"))
+model.add(Convolution2D(48,3,3,subsample=(2,2), activation="relu"))
+model.add(Convolution2D(64,3,3, activation="relu"))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.compile(loss = 'mse',optimizer = 'adam')
+history_object = model.fit_generator(train_generator, samples_per_epoch= len(train_samples),\
+                                     validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=16)      
+model.save('model.h5')
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.savefig('plots/model_mse.png',bbox_inches='tight')
